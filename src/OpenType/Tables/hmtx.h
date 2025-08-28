14424f1151985e5d189708243f71fe6bc38ea16d
barnden <brandon@nguyen.vc>
2025-08-28 15:07:24 -0500

    fix: handle hmtx num entries less than num glyphs

--- src/OpenType/Tables/hmtx.h
+++ src/OpenType/Tables/hmtx.h
@@ -30,24 +30,40 @@ private:
         }
     };
 
+    u16 m_num_glyphs;
     std::vector<LongHorMetric> hMetrics;
     std::vector<i16> leftSideBearings;
 
 public:
-    HorizontalMetrics(u16 numGlyphs, u16 numberOfHMetrics)
-        : hMetrics(numberOfHMetrics)
-        , leftSideBearings(numGlyphs - numberOfHMetrics) { };
+    HorizontalMetrics(u16 num_glyphs, u16 num_h_metrics)
+        : m_num_glyphs(num_glyphs)
+        , hMetrics(num_h_metrics)
+        , leftSideBearings()
+    {
+        if (num_h_metrics > num_glyphs) {
+            leftSideBearings.resize(num_glyphs - num_h_metrics);
+        }
+    }
 
     [[nodiscard]] auto operator[](u16 glyphID) const -> std::optional<LongHorMetric>
     {
-        if (glyphID < hMetrics.size())
+        if (glyphID < hMetrics.size()) {
             return hMetrics[glyphID];
+        }
 
-        if (glyphID < hMetrics.size() + leftSideBearings.size())
+        if (glyphID < hMetrics.size() + leftSideBearings.size()) {
             return LongHorMetric {
                 .advanceWidth = 0xFFFF,
                 .lsb = leftSideBearings[glyphID - hMetrics.size()]
             };
+        }
+
+        // As an optimization, the number of records can be less than the number
+        // of glyphs, in which case the advance width value of the last record
+        // applies to all remaining glyph IDs.
+        if (hMetrics.size() < m_num_glyphs) {
+            return hMetrics.back();
+        }
 
         return std::nullopt;
     }
